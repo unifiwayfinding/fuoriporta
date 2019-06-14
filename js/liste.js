19278859152059cf573f55d90c074f03cf449229
Jacopo <jacoping@gmail.com>
2019-06-14 12:43:40 +0200

    aggiornato "liste.js"

--- js/liste.js
+++ js/liste.js
@@ -1,10 +1,90 @@
 var lista_strutture = [
-  {
+    {
+  "option_value": "DipBIO",
+  "option_label": "BIO",
+  "struttura_bold_1": "BIO",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Biologia"
+},
+{
+  "option_value": "DipCHIMICA",
+  "option_label": "CHIMICA",
+  "struttura_bold_1": "CHIMICA",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Chimica Ugo Schiff"
+},
+{
+  "option_value": "DipDAGRI",
+  "option_label": "DAGRI",
+  "struttura_bold_1": "DAGRI",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Scienze e Tecnologie Agrarie,",
+  "struttura_light_3": "Alimentari, Ambientali e Forestali"
+},
+{
+  "option_value": "DipDICEA",
+  "option_label": "DICEA",
+  "struttura_bold_1": "DICEA",
+  "struttura_light_1": "Dipartimento di Ingegneria",
+  "struttura_light_2": "Civile e Ambientale"
+},
+{
+  "option_value": "DipDIDA",
+  "option_label": "DIDA",
+  "struttura_bold_1": "DIDA",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Architettura"
+},
+{
+  "option_value": "DipDIEF",
+  "option_label": "DIEF",
+  "struttura_bold_1": "DIEF",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Ingegneria Industriale"
+},
+{
+  "option_value": "DipDILEF",
+  "option_label": "DILEF",
+  "struttura_bold_1": "DILEF",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Lettere e Filosofia"
+},
+{
+  "option_value": "DipDIMAI",
+  "option_label": "DIMAI",
+  "struttura_bold_1": "DIMAI",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Matematica e Informatica",
+  "struttura_light_3": "Ulisse Dini"
+},
+{
+  "option_value": "DipDINFO",
+  "option_label": "DINFO",
+  "struttura_bold_1": "DINFO",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Ingegneria dell'Informazione"
+},
+{
   "option_value": "DipDISEI",
   "option_label": "DISEI",
   "struttura_bold_1": "DISEI",
   "struttura_light_1": "Dipartimento di",
   "struttura_light_2": "Scienze per l'Economia e per l'Impresa"
+},
+{
+  "option_value": "DipDISIA",
+  "option_label": "DISIA",
+  "struttura_bold_1": "DISIA",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Statistica, Informatica, Applicazioni",
+  "struttura_light_3": "G. Parenti"
+},
+{
+  "option_value": "DipDMSC",
+  "option_label": "DMSC",
+  "struttura_bold_1": "DMSC",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Medicina Sperimentale e Chimica"
 },
 {
   "option_value": "DipDSG",
@@ -14,6 +94,14 @@ var lista_strutture = [
   "struttura_light_2": "Scienze Giuridiche"
 },
 {
+  "option_value": "DipDSBSC",
+  "option_label": "DSBSC",
+  "struttura_bold_1": "DSBSC",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Scienze Biomediche, Sperimentali e",
+  "struttura_light_3": "Cliniche Mario Serio"
+},
+{
   "option_value": "DipDSPS",
   "option_label": "DSPS",
   "struttura_bold_1": "DSPS",
@@ -21,10 +109,100 @@ var lista_strutture = [
   "struttura_light_2": "Scienze Politiche e Sociali"
 },
 {
+  "option_value": "DipDSS",
+  "option_label": "DSS",
+  "struttura_bold_1": "DSS",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Scienze della Salute"
+},
+{
+  "option_value": "DipDST",
+  "option_label": "DST",
+  "struttura_bold_1": "DST",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Scienze della Terra"
+},
+{
+  "option_value": "DipFISICA",
+  "option_label": "FISICA",
+  "struttura_bold_1": "FISICA",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Fisica e Astronomia"
+},
+{
+  "option_value": "DipFORLIPSI",
+  "option_label": "FORLIPSI",
+  "struttura_bold_1": "FORLIPSI",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Formazione, Lingue, Intercultura,",
+  "struttura_light_3": "Letteratura e Psicologia"
+},
+{
+  "option_value": "DipNEUROFARBA",
+  "option_label": "NEUROFARBA",
+  "struttura_bold_1": "NEUROFARBA",
+  "struttura_light_1": "Dipartimento di",
+  "struttura_light_2": "Formazione, Lingue, Intercultura,",
+  "struttura_light_3": "Letteratura e Psicologia"
+},
+{
+  "option_value": "DipSAGAS",
+  "option_label": "SAGAS",
+  "struttura_bold_1": "SAGAS",
+  "struttura_light_1": "Dipartimento di Storia,",
+  "struttura_light_2": "Archeologia, Geografia,",
+  "struttura_light_3": "Arte e Spettacolo"
+},
+{
+  "option_value": "ScAgraria",
+  "option_label": "Scuola di Agraria",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Agraria",
+},
+{
+  "option_value": "ScArchitettura",
+  "option_label": "Scuola di Architettura",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Architettura",
+},
+{
+  "option_value": "ScEconomia",
+  "option_label": "Scuola di Economia e Management",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Economia e",
+  "struttura_bold_3": "Management",
+},
+{
   "option_value": "ScGiuripsrudenza",
   "option_label": "Scuola di Giuripsrudenza",
   "struttura_bold_1": "Scuola di",
   "struttura_bold_2": "Giurisprudenza",
+},
+{
+  "option_value": "ScIngegneria",
+  "option_label": "Scuola di Ingegneria",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Ingegneria",
+},
+{
+  "option_value": "ScPsicologia",
+  "option_label": "Scuola di Psicologia",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Psicologia",
+},
+{
+  "option_value": "ScScienzeSaluteUmana",
+  "option_label": "Scuola di Scienze della Salute Umana",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Scienze della",
+  "struttura_bold_3": "Salute Umana",
+},
+{
+  "option_value": "ScScienzeMFN",
+  "option_label": "Scuola di Scienze Matematiche, Fisiche e Naturali",
+  "struttura_bold_1": "Scuola di",
+  "struttura_bold_2": "Scienze Matematiche",
+  "struttura_bold_3": "Fisiche e Naturali",
 },
 {
   "option_value": "ScScienzePolitiche",
@@ -34,11 +212,11 @@ var lista_strutture = [
   "struttura_bold_3": "Cesare Alfieri",
 },
 {
-  "option_value": "ScEconomia",
-  "option_label": "Scuola di Economia e Management",
+  "option_value": "ScStudiUmanistici",
+  "option_label": "Scuola di Studi Umanistici e della Formazione",
   "struttura_bold_1": "Scuola di",
-  "struttura_bold_2": "Economia e",
-  "struttura_bold_3": "Management",
+  "struttura_bold_2": "Studi Umanistici",
+  "struttura_bold_3": "e della Formazione",
 },
 {
   "option_value": "altro",
